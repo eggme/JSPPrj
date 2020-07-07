ebfa34e411c09a150df195457c7eac6d96079ea5
kyyeto9984 <kyyeto9984@naver.com>
2020-07-07 18:45:19 +0900

    GET 요청 추가

--- src/com/lsj/web/Nana.java
+++ src/com/lsj/web/Nana.java
@@ -4,18 +4,23 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/hello1")
 public class Nana extends HttpServlet{
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter writer = resp.getWriter();
-		writer.print("Hello~ !!! asdsad!");
+		String cnt = req.getParameter("cnt");
+		if( cnt != null && !cnt.equals("")) {
+			for(int i=0;i<Integer.parseInt(cnt);i++)
+				writer.println("¾È³ç Servlet <br />");
+		}
 	}
-
-	
-	
 }
